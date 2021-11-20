87d78f99152e3f848627f6154b2aa497ed8f6bd1
Hocket Apersil <91352832+Hocket1@users.noreply.github.com>
2021-11-20 01:18:41 +0000

    Create InputLenPrintFunction.py

---
+++ InputLenPrintFunction.py
@@ -0,0 +1,8 @@
+# Program to count the length of a string from an input function
+a = input('enter your name here ')
+print(len(a))
+
+#Another geek with the input function
+c = input('enter your name here ')
+d = 'Hello '
+print(d, c)
