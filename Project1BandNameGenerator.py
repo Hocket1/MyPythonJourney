c39bee9bdff68c88cc2d88fa0ffeb957a0a54cbc
Hocket Apersil <91352832+Hocket1@users.noreply.github.com>
2021-11-20 02:41:10 +0000

    Update Project1BandNameGenerator.py

--- Project1BandNameGenerator.py
+++ Project1BandNameGenerator.py
@@ -1,8 +1,8 @@
 #Bandname Generator Project
 print('Welcome to my Bandname Generator') #Create a greeting for my program
 
-a = input('Please enter the name of the city you grew up in:\n ') #Ask the user to enter the name of their city
+city = input('Please enter the name of the city you grew up in:\n ') #Ask the user to enter the name of their city
 
-b = input('Please enter the name of a pet:\n ') #Ask the user to enter the name of a pet
+pet = input('Please enter the name of a pet:\n ') #Ask the user to enter the name of a pet
 
-print('You bandname is ' + a, b) # combine the name of their city and pet to generate their bandname.
+print('You bandname is ' + city, pet) # combine the name of their city and pet to generate their bandname.
